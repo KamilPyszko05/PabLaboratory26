acb9263e4548e1c5dc653c4000e942c7ea889eb5
kamil.pyszko <kamil.pyszko@gmail.com>
2026-03-11 10:19:20 +0100

    without tests(start of lab3)

--- Infrastructure/Memory/MemoryGenericRepository.cs
+++ Infrastructure/Memory/MemoryGenericRepository.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.Memory;
 public class MemoryGenericRepository<T>: IGenericRepositoryAsync<T> 
     where T: class 
 {
-    private Dictionary<Guid, T> _data = new();
+    protected Dictionary<Guid, T> _data = new();
     
     public Task<T?> FindByIdAsync(Guid id)
     {
@@ -15,17 +15,41 @@ public class MemoryGenericRepository<T>: IGenericRepositoryAsync<T>
 
     public Task<List<T>> FindAllAsync()
     {
-        ...
+        var result = _data.Values.ToList();
+        return Task.FromResult(result);
+    }
+
+    public Task AddAsync(Guid id, T entity)
+    {
+        if (!_data.ContainsKey(id))
+        {
+            _data.Add(id, entity);
+        }
+        return Task.CompletedTask;
     }
 
     public Task RemoveByIdAsync(Guid id)
     {
-        ...
+        if (!_data.ContainsKey(id))
+        {
+            throw new KeyNotFoundException($"Nie znaleziono encji o id: {id}");
+        }
+        _data.Remove(id);
+        return Task.CompletedTask;
     }
 
-    public Task UpdateAsync(Guid id, T o)
+    public Task UpdateAsync(Guid id, T entity)
     {
-        ...        
+        if (!_data.ContainsKey(id))
+        {
+            throw new KeyNotFoundException($"Nie można zaktualizować. Brak encji o id: {id}");
+        }
+        _data[id] = entity;
+        return Task.CompletedTask;
     }
-    // pozostałe metody
+
+    public Task<bool> ExistsAsync(Guid id)
+    {
+        return Task.FromResult(_data.ContainsKey(id));
+    }
 }
